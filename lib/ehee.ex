5230f9d9c4d0af7b67e2d1c625171b475efc86b0
kanmo <kanmo.ak@gmail.com>
2016-09-25 21:12:41 +0900

    implement pagination option

--- lib/ehee.ex
+++ lib/ehee.ex
@@ -26,12 +26,69 @@ defmodule Ehee do
     |> handle_response
   end
 
-  # TODO body -> params
-  def get(path, credential, body \\ []) do
+  def get(path, credential, params \\ [], options \\ []) do
     url = url(path)# |> merge_uri_params
-    request!(:get, url, Poison.encode!(body), authorization_headers(credential.auth, []), [])
-    |> handle_response
+    case pagination(options) do
+      :nil -> request_stream(:get, url, credential)
+      :stream -> request_stream(:get, url, credential)
+    end
   end
+
+  defp pagination(options) do
+    Keyword.get(options, :pagination, :nil)
+  end
+
+  defp request_stream(method, url, credential, body \\ "", override \\ nil) do
+    request_with_pagination(method, url, credential.auth, body)
+    |> stream_if_needed(override)
+  end
+
+  defp stream_if_needed(result = {status_code, _}, _) when is_number(status_code), do: result
+  defp stream_if_needed({body, nil, _}, _), do: body
+  defp stream_if_needed({body, _, _}, :one_page), do: body
+  defp stream_if_needed(initial_results, _) do
+    Stream.resource(
+      fn -> initial_results end,
+      &handle_stream/1,
+      fn _ -> nil end)
+  end
+
+  def request_with_pagination(method, url, auth, body \\ "") do
+    resp = request!(method, url, Poison.encode!(body), authorization_headers(auth, []), [])
+    case handle_response(resp) do
+      x when is_tuple(x) -> x
+      _ -> pagination_tuple(resp, auth)
+    end
+  end
+
+  defp handle_stream({[], nil, _}), do: {:halt, nil}
+  defp handle_stream({[], next, auth}) do
+    request_with_pagination(:get, next, auth, "")
+    |> handle_stream
+  end
+  # defp handle_stream({items, next, auth}) when is_list(items) do
+  #   {items, {[], next, auth}}
+  # end
+  defp handle_stream({item, next, auth}) do
+    {[item], {[], next, auth}}
+  end
+
+  defp pagination_tuple(%HTTPoison.Response{headers: headers} = resp, auth) do
+    {handle_response(resp), next_link(headers), auth}
+  end
+
+  defp next_link(headers) do
+    for {"Link", link_header} <- headers, links <- String.split(link_header, ",") do
+      Regex.named_captures(~r/<(?<link>.*)>;\s*rel=\"(?<rel>.*)\"/, links)
+      |> case do
+        %{"link" => link, "rel" => "next"} -> link
+        _ -> nil
+      end
+    end
+    |> Enum.filter(&(not is_nil(&1)))
+    |> List.first
+  end
+
 
   defp merge_uri_params(uri, []), do: uri
 
--- lib/ehee/gists.ex
+++ lib/ehee/gists.ex
@@ -14,10 +14,9 @@ defmodule Ehee.Gists do
     get("/users/#{username}/gists", credential)
   end
 
-  # def gists_public do
-  #   Stream.resource(fn -> fetch_gists("/gists/public") end, &process_page/1, fn _ -> end)
-  #   |> Enum.take(100)
-  # end
+  def gists_public(credential) do
+    get("/gists/public", credential, [], [pagination: :stream])
+  end
 
   def gists_create(credential, description, is_public, file_name, content) do
     body = %{ "description": description,
@@ -82,46 +81,14 @@ defmodule Ehee.Gists do
     delete("/gists/#{gist_id}/comments/#{comment_id}",  credential)
   end
 
-  defp fetch_gists(url) do
-    response = Gateway.get(url) |> elem(1)
-    gists = Poison.decode!(response.body)
+  defp fetch_gists(cred, url) do
+    gists = get(url)
 
     links = response.headers
     |> extract_links
     |> parse_links
 
     {gists, links["next"]}
-  end
-
-  defp extract_links(headers) do
-    [{_, value} | _] = Enum.filter(headers, &(elem(&1, 0) == "Link"))
-    value
-  end
-
-  defp parse_links(links_string) do
-    links = String.split(links_string, ", ")
-
-    Enum.map(links, fn link ->
-      [_,name] = Regex.run(~r{rel="([a-z]+)"}, link)
-      [_,url] = Regex.run(~r{<([^>]+)>}, link)
-      short_url = String.replace(url, Gateway.endpoint, "")
-
-      {name, short_url}
-    end) |> Enum.into(%{})
-  end
-
-  defp process_page({nil, nil}) do
-    {:halt, nil}
-  end
-
-  defp process_page({nil, next_page_url}) do
-    next_page_url
-    |> fetch_gists
-    |> process_page
-  end
-
-  defp process_page({items, next_page_url}) do
-    {items, {nil, next_page_url}}
   end
 
 end
