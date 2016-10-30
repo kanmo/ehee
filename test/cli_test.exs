6c6fb7285b6aee161b240b55508c526557ef2bd3
kanmo <kanmo.ak@gmail.com>
2016-10-30 20:54:37 +0900

    Fix cli test

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -10,19 +10,17 @@ defmodule CliTest do
   end
 
   test "gists method name returned by option parsing with -g and --gist options" do
-    assert parse_args(["-g", "list_users"]) == { :gist, "list_users" }
-    assert parse_args(["--gist", "list_users"]) == { :gist, "list_users" }
+    assert parse_args(["-g", "list_users"]) == { :gist, "list_users", [] }
+    assert parse_args(["--gist", "list_users"]) == { :gist, "list_users", [] }
   end
 
   test "repository method name returned by option parsing with -r and --repository options" do
-    assert parse_args(["-r", "list_users"]) == { :repository, "list_users" }
-    assert parse_args(["--repository", "list_users"]) == { :repository, "list_users" }
+    assert parse_args(["-r", "list_users"]) == { :repository, "list_users", [] }
+    assert parse_args(["--repository", "list_users"]) == { :repository, "list_users", [] }
   end
 
   test "three values" do
     assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
   end
-
-
 
 end
